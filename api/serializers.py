add1892875cd059caa837022e114fa4275857bbd
Emmanuel Ogbizi-Ugbe <>
2018-03-24 15:23:33 -0400

    custom serializer for nested representation

--- api/serializers.py
+++ api/serializers.py
@@ -2,16 +2,24 @@ from rest_framework import serializers
 from .models import Package, Status
 
 
+class StatusSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+        model = Status
+        fields = ('id', 'package', 'latitude', 'longitude', 'elevation',)
+
+
+class PackageStatusSerializer(StatusSerializer):
+    def to_representation(self, instance):
+        # get the default representation
+        ret = super().to_representation(instance)
+        ret.pop('package', None)
+        return ret
+
+
 class PackageSerializer(serializers.ModelSerializer):
-    tracking = serializers.HyperlinkedRelatedField(
-        many=True, read_only=True, view_name='status-detail')
+    tracking = PackageStatusSerializer(many=True, read_only=True)
 
     class Meta:
         model = Package
         fields = ('id', 'description', 'tracking',)
-
-
-class StatusSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Status
-        fields = ('package', 'latitude', 'longitude', 'elevation',)
