81d99adea017f4d07d5e36e4a925be91985b9eeb
Emmanuel Ogbizi-Ugbe <>
2018-03-24 13:37:34 -0400

    format model.py

--- api/models.py
+++ api/models.py
@@ -1,27 +1,29 @@
 from django.db import models
 
 
-"""
-Model defining a package
-.id - Unique identifier for package, auto generated on insert.
-.description - Description of package.
-"""
 class Package(models.Model):
+    """
+    Model defining a package
+    .id - Unique identifier for package, auto generated on insert.
+    .description - Description of package.
+    """
+
     description = models.CharField(max_length=140, blank=False)
 
     class Meta:
         ordering = ('id',)
 
 
-"""
-Status of a package
-.created = Server time of the status update
-.package = Foreign key relationship to package (package_id)
-.latitude = Latitude of package to 8 decimal places
-.longitude = Longitude of package to 8 decimal places
-.elevation = Vertical elevation of package in metres to 3 decimal places (millimetre)
-"""
 class Status(models.Model):
+    """
+    Status of a package
+    .created = Server time of the status update
+    .package = Foreign key relationship to package (package_id)
+    .latitude = Latitude of package to 8 decimal places
+    .longitude = Longitude of package to 8 decimal places
+    .elevation = Vertical elevation of package in metres to 3 decimal places (millimetre)
+    """
+
     created = models.DateTimeField(auto_now_add=True)
     created = models.DateTimeField(editable=False)
     package = models.ForeignKey(Package, on_delete=models.PROTECT)
