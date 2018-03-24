a04f0e86c3025620891a7c97c12db375efd12c74
Emmanuel Ogbizi-Ugbe <>
2018-03-24 11:10:15 -0400

    make model and migrations for api

--- api/models.py
+++ api/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+"""
+Model defining a package
+.id - Unique identifier for package, auto generated on insert.
+.description - Description of package.
+"""
+class Package(models.Model):
+    description = models.CharField(max_length=140, blank=False)
+
+    class Meta:
+        ordering = ('id',)
+
+
+"""
+Status of a package
+.created = Server time of the status update
+.package = Foreign key relationship to package (package_id)
+.latitude = Latitude of package to 8 decimal places
+.longitude = Longitude of package to 8 decimal places
+.elevation = Vertical elevation of package in metres to 3 decimal places (millimetre)
+"""
+class Status(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    package = models.ForeignKey(Package, on_delete=models.PROTECT)
+    latitude = models.DecimalField(max_digits=12, decimal_places=8)
+    longitude = models.DecimalField(max_digits=12, decimal_places=8)
+    elevation = models.DecimalField(max_digits=7, decimal_places=3)
+
+    class Meta:
+        ordering = ('package_id','created',)
