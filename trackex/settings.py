2b8c7fd1f36397afb6aa09c649155bb3f84f2167
Emmanuel Ogbizi-Ugbe <>
2018-03-24 17:33:44 -0400

    add allowed hosts for prettier urls

--- trackex/settings.py
+++ trackex/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'demo-texada.app']
 
 
 # Application definition
