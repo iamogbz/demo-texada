316a6a88cec304b7679392c14c18a7df38a52241
Emmanuel Ogbizi-Ugbe <>
2018-03-24 14:26:46 -0400

    add views to the browsable api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from .models import Package, Status
+from rest_framework import viewsets
+from .serializers import PackageSerializer, StatusSerializer
 
-# Create your views here.
+
+class PackageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows packages to be viewed or edited.
+    """
+    queryset = Package.objects.all()
+    serializer_class = PackageSerializer
+
+
+class StatusViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows status to be created and viewed.
+    """
+    queryset = Status.objects.all()
+    serializer_class = StatusSerializer
--- trackex/urls.py
+++ trackex/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from quickstart import views
+from api import views as api_views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'packages', api_views.PackageViewSet)
+router.register(r'tracking', api_views.StatusViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
