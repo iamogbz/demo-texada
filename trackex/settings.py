c1b74c63328f61f9e293e60c835d068f0e117d9e
Emmanuel Ogbizi-Ugbe <>
2018-03-24 14:27:25 -0400

    fix numbers being serialized as strings

--- trackex/settings.py
+++ trackex/settings.py
@@ -115,7 +115,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'COERCE_DECIMAL_TO_STRING': False,
 }
 
 
