66f202ae892344f6d03d73ceb0426042bca17662
Emmanuel Ogbizi-Ugbe <>
2018-03-24 18:26:41 -0400

    allow update of user passwords

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,11 +1,23 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().update(instance, validated_data)
+
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'username', 'email', 'groups', 'password')
+        extra_kwargs = {'password': {'write_only': True,
+                                     'style': {'input_type': 'password'}}}
 
 
 class GroupSerializer(serializers.HyperlinkedModelSerializer):
