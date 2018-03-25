ef1d6e72f747d85fa23b8f04f34b7be0841f0e28
Emmanuel Ogbizi-Ugbe <>
2018-03-24 20:51:05 -0400

    only update password when field is updated

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -5,18 +5,25 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
+    def secure_password(self, validated_data):
+        if "password" in validated_data:
+            pw = validated_data['password']
+            validated_data['password'] = make_password(pw)
+        return validated_data
+
     def create(self, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        return super().create(validated_data)
+        return super().create(self.secure_password(validated_data))
 
     def update(self, instance, validated_data):
-        validated_data['password'] = make_password(validated_data['password'])
-        return super().update(instance, validated_data)
+        if "password" in validated_data:
+            pw = validated_data['password']
+            validated_data['password'] = make_password(pw)
+        return super().update(instance, self.secure_password(validated_data))
 
     class Meta:
         model = User
         fields = ('url', 'username', 'email', 'groups', 'password')
-        extra_kwargs = {'password': {'write_only': True,
+        extra_kwargs = {'password': {'write_only': True, 'required': False,
                                      'style': {'input_type': 'password'}}}
 
 
