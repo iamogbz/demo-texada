200e247318c6b0475dd6b27d8a126e7c31d90375
Emmanuel Ogbizi-Ugbe <>
2018-03-25 17:25:31 -0400

    package model tests

--- api/tests.py
+++ api/tests.py
@@ -1,8 +1,14 @@
 import os
 import prepare
+import string
+import random
 
 from django.test import SimpleTestCase
 from django.test import TestCase
+from django.core import exceptions
+from django.db import transaction
+from django.db import utils
+from django.core import exceptions
 from .models import *
 
 
@@ -40,3 +46,51 @@ class EnviromentTest(SimpleTestCase):
         fail_msg = 'missing "DB_USER" in .env'
         self.assertIsNotNone(value, fail_msg)
         self.assertIsNot(value, '', fail_msg)
+
+
+class PackageModelTest(TestCase):
+    """
+    Test package model and methods
+    """
+
+    def test_can_build_model(self):
+        description = 'short description'
+        package = Package(description=description)
+        self.assertIsNotNone(package)
+        self.assertEqual(package.description, description)
+
+    def test_description_size_limit(self):
+        max_len = 140
+        long_desc = ''.join(random.choice(string.ascii_lowercase)
+                            for x in range(max_len + 1))
+        package = Package(description=long_desc)
+        try:
+            package.save()
+            self.fail('Descriptors longer than '+max_len+' chars allowed')
+        except utils.DataError:
+            pass
+
+    def test_can_persist_model(self):
+        package = Package(description='short description')
+        package.save()
+        test_pkg = Package(package.pk)
+        test_pkg.refresh_from_db()
+        self.assertEqual(package.description, test_pkg.description)
+        new_desc = 'new description'
+        package.description = new_desc
+        package.save()
+        test_pkg.refresh_from_db()
+        self.assertEqual(test_pkg.description, new_desc)
+
+    def test_can_delete_model(self):
+        package = Package(description='unnec')
+        package.save()
+        test_pkg = Package(package.pk)
+        test_pkg.refresh_from_db()
+        self.assertEqual(package.description, test_pkg.description)
+        package.delete()
+        try:
+            test_pkg.refresh_from_db()
+            self.fail('Package was not deleted')
+        except exceptions.ObjectDoesNotExist:
+            pass
