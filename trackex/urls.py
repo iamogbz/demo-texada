c55b8defd4986f3515ac34d2f1063b18abb0bb4d
Emmanuel Ogbizi-Ugbe <>
2018-03-25 21:50:22 -0400

    remove quickstart from project

--- trackex/urls.py
+++ trackex/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from quickstart import views
 from api import views as api_views
 
 router = routers.DefaultRouter(trailing_slash=False)
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 router.register(r'status', api_views.StatusViewSet)
 router.register(r'packages', api_views.PackageViewSet)
 
