ea84a10b8dcfdbf96c9761e7e143bdfabb921605
Emmanuel Ogbizi-Ugbe <>
2018-03-27 16:51:48 -0400

    run wsgi.py during test prep to improve coverage

--- prepare.py
+++ prepare.py
@@ -3,7 +3,7 @@ import os
 import django
 from dotenv import load_dotenv
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trackex.settings")
+from trackex import wsgi
 
 load_dotenv(verbose=True, dotenv_path='.env')
 django.setup()
